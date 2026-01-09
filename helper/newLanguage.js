57cb5c9028beea7dd825c0503e12067f475c1a25
Sanchit saini <sanchitsaini46@gmail.com>
2026-01-09 13:43:56 +0530

    hinglish words add

--- helper/newLanguage.js
+++ helper/newLanguage.js
@@ -1,15 +1,26 @@
 const hinglish = {
     labels: {
-        'stupide': -2,
+        //positive words
+        'jhakas': 3,
         'mast': 3,
         'badhiya': 2,
-        'bekaar': -2,
-        'ghatiya': -3,
-        'pagal': -2,
         'bhai': 1,
         'love': 3,
-        'hate': -3,
-        'lala': 1
+        'lala': 1,
+        'ek number':3,
+        //negative words
+         'bekaar': -2,
+         'ghatiya': -3,
+         'hate': -3,
+         'pagal': -1,
+         'chutiya':-3,
+         'gandu': -3,
+         'third class':-2,
+         'gande' : -1,
+         
+
+
+
 
     }
 }
